5eff6bdb005ad3797928c4a9d50efc48a3e47dc0
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2022-02-12 11:46:09 -0500

    For

--- src/main.go
+++ src/main.go
@@ -2,30 +2,24 @@ package main
 
 import "fmt"
 
-func normalFunction(message string) {
-	fmt.Println(message)
-
-}
-
-func triplaArgument(a, b int, c string) {
-	fmt.Println(a, b, c)
-}
-
-func returnValue(a int) int {
-	return a * 2
-}
-
-func doubleReturn(a int) (c int, d int) {
-	return a, a * 2
-}
-
 func main() {
 
-	normalFunction("hola mundo")
-	triplaArgument(1, 2, "hola")
-	value := returnValue(2)
-	fmt.Println("value: ", value)
+	//For conditional
+	for i := 0; i <= 10; i++ {
+		fmt.Println(i)
+	}
 
-	value1, value2 := doubleReturn(2)
-	fmt.Println("Value1 ", value1, "Value2 ", value2)
+	// For while
+	counter := 0
+	for counter < 10 {
+		fmt.Println(counter)
+		counter++
+	}
+
+	// For forever
+	counterForever := 0
+	for {
+		fmt.Println(counterForever)
+		counterForever++
+	}
 }
