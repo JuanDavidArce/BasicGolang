a5e7625d29ae6bd21b29377fd6e5280cc12207bf
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2022-02-12 10:31:37 -0500

    Operator

--- src/main.go
+++ src/main.go
@@ -4,30 +4,39 @@ import "fmt"
 
 func main() {
 
-	// Constants declaration
-	const pi float64 = 3.14
-	const pi2 = 3.1415
-
-	fmt.Println("Pi", pi)
-	fmt.Println("pi2", pi2)
-
-	// Variables declaration
-	base := 12
-	var altura int = 14
-	var area int
-
-	fmt.Println(base, altura, area)
-
-	// Zero Values
-	var a int
-	var b float64
-	var c string
-	var d bool
-
-	fmt.Println(a, b, c, d)
-
 	// Area Cuadrado
 	const baseCuadrado = 10
 	areaCuadrado := baseCuadrado * baseCuadrado
 	fmt.Println("area cuadrado: ", areaCuadrado)
+
+	x := 10
+	y := 50
+
+	// Suma
+	result := x + y
+	fmt.Println("suma ", result)
+
+	// Rexta
+	result = y - x
+	fmt.Println("Resta", result)
+
+	// Multiplicacion
+	result = x * y
+	fmt.Println("multiplicacion:", result)
+
+	// Division
+	result = y / x
+	fmt.Println("division: ", result)
+
+	// Modulo
+	result = y % x
+	fmt.Println("Modulo: ", result)
+
+	// Incremental
+	x++
+	fmt.Println("Incremental:", x)
+
+	// Decremental
+	x--
+	fmt.Println("Decremental: ", x)
 }
