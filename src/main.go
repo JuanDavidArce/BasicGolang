13bc531f798962e8ef9eae8d92ead64a73a68b81
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2022-02-12 10:21:24 -0500

    zero values,constant and variable declaration

--- src/main.go
+++ src/main.go
@@ -3,5 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hola mundo")
+
+	// Constants declaration
+	const pi float64 = 3.14
+	const pi2 = 3.1415
+
+	fmt.Println("Pi", pi)
+	fmt.Println("pi2", pi2)
+
+	// Variables declaration
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println(base, altura, area)
+
+	// Zero Values
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	// Area Cuadrado
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println("area cuadrado: ", areaCuadrado)
 }
