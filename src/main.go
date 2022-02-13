9786aa3be824ebcb7d4231174cd618d1279f0fe3
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2022-02-13 09:38:02 -0500

    traverse an array and strings

--- src/main.go
+++ src/main.go
@@ -1,31 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func isPalindromo(text string) {
+	text = strings.ToLower(text)
+	var textReverse string
+	for i := len(text) - 1; i >= 0; i-- {
+		textReverse += string(text[i])
+
+	}
+
+	if textReverse == text {
+		fmt.Println("Es palindromo")
+	} else {
+		fmt.Println("No es palindromo")
+	}
+}
 
 func main() {
-	// Array
-	var array [4]int
-	array[0] = 1
-	array[1] = 2
-	fmt.Println(array, len(array), cap(array))
+	slice := []string{"hola", "que", "hace"}
 
-	// slice
-	slice := []int{0, 1, 2, 3, 4, 5, 6}
-	fmt.Println(slice, len(slice), cap(slice))
-
-	// Metodos en el slice
-	fmt.Println(slice[0])
-	fmt.Println(slice[:3])
-	fmt.Println(slice[2:4])
-	fmt.Println(slice[4:])
-
-	// Append
-	slice = append(slice, 7)
-	fmt.Println(slice)
-
-	// Append new list
-	newSlice := []int{8, 9, 10}
-	slice = append(slice, newSlice...)
-	fmt.Println(slice)
+	for i, valor := range slice {
+		fmt.Println(i, valor)
+	}
+	isPalindromo("Amor a roma")
 
 }
