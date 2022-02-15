e6201fd2e0fb7b55c5aa26addadeacbdfb2fff11
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2022-02-14 22:46:02 -0500

    Chanels:

--- src/main.go
+++ src/main.go
@@ -4,13 +4,37 @@ import (
 	"fmt"
 )
 
-func say(text string, c chan string) {
+func message(text string, c chan string) {
 	c <- text
 }
 
 func main() {
-	c := make(chan string, 1)
-	fmt.Println("Hello")
-	go say("Bye", c)
-	fmt.Println(<-c)
+	c := make(chan string, 2)
+	c <- "Mensaje 1"
+	c <- "Mensaje 2"
+
+	fmt.Println(len(c), cap(c))
+
+	// Range and close
+	close(c)
+	// c <- "Mensaje 3"
+	for message := range c {
+		fmt.Println(message)
+	}
+
+	// Select
+	email1 := make(chan string)
+	email2 := make(chan string)
+	go message("Mensaje 1", email1)
+	go message("Mensaje 2", email2)
+
+	for i := 0; i < 2; i++ {
+		select {
+		case m1 := <-email1:
+			fmt.Println("Email recibido de email1", m1)
+		case m2 := <-email2:
+			fmt.Println("Email recibido de email2", m2)
+
+		}
+	}
 }
